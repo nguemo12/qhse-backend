111d9c34c43710d6812a4a7931d2cf95c5e0f52a
Aymard <yakubaba1234@gmail.com>
2023-07-25 10:00:41 +0100

    deds

--- controllers/chatgpt.js
+++ controllers/chatgpt.js
@@ -9,7 +9,7 @@ module.exports = function (req){
     return new Promise(async(resolve,rejected)=>{
        
         const configuration = new Configuration({
-            apiKey: config.CHAT_GPT_API_KEY,
+            apiKey: "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",
         });
 
           const openai = new OpenAIApi(configuration);
